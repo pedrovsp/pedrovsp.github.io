fcbb99399cb614646b6ab65101f042d0e55e97fa
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-09-04 11:44:11 -0300

    feat: implement h2 and h3 component

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -22,6 +22,25 @@ export const H1 = (props: TitleProps) => {
 export const H2 = (props: TitleProps) => {
 	return (
 		<h2 className='title'>
+			<span className='text'>
+				{props.title}
+			</span>
+			<span className='emoji'>
+				{props.emoji}
+			</span>
 		</h2>
+	)
+}
+
+export const H3 = (props: TitleProps) => {
+	return (
+		<h3 className='title'>
+			<span className='text'>
+				{props.title}
+			</span>
+			<span className='emoji'>
+				{props.emoji}
+			</span>
+		</h3>
 	)
 }
