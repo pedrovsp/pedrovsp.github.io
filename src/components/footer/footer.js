a64d8a3931e09b87b70adc924fbb0018d23d7b68
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-09-04 13:02:07 -0300

    fix: link usage

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,16 +2,15 @@ import React from "react"
 import "./footer.scss"
 import Github from "../../assets/icons/github.svg"
 import Linkedin from "../../assets/icons/linkedin.svg"
-import { Link } from 'gatsby'
 
 const Footer = () => (
   <footer className='footer'>
-    <Link to="https://github.com/pedrovsp" target="_blank">
+    <a href="https://github.com/pedrovsp" target="_blank" rel="noreferrer">
       <Github />
-    </Link>
-    <Link to="https://linkedin.com/in/pedrovsp" target="_blank">
+    </a>
+    <a href="https://linkedin.com/in/pedrovsp" target="_blank" rel="noreferrer">
       <Linkedin />
-    </Link>
+    </a>
   </footer>
 )
 
