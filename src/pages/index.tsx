03f5605c6a92692af380af27bba217196157a8c1
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-09-04 22:41:05 -0300

    feat: add figs to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,31 +7,47 @@ import { H1, H2, H3 } from '../components/title/title'
 import { Par } from "../components/paragraph/paragraph"
 import { BoxList } from '../components/box-list/box-list'
 import { ContentBox } from '../components/content-box/content-box';
+import { Geoform } from "../components/geoform/geoform";
 
-const contents: {  title: string; type: 'article' | 'talk' | 'project'; content: string; url: string;}[] = [
-  { type: 'article', title: 'O minimo sobre Git', url:'https://www.google.com', content:'sometings'},
-  { type: 'article', title: 'O minimo sobre Git', url:'https://www.google.com', content:'sometings'},
-  { type: 'article', title: 'O minimo sobre Git', url:'https://www.google.com', content:'sometings'},
+const contents: { title: string; type: 'article' | 'talk' | 'project'; content: string; url: string; }[] = [
+  { type: 'article', title: 'O minimo sobre Git', url: 'https://www.google.com', content: 'sometings' },
+  { type: 'article', title: 'O minimo sobre Git', url: 'https://www.google.com', content: 'sometings' },
+  { type: 'article', title: 'O minimo sobre Git', url: 'https://www.google.com', content: 'sometings' },
 ];
 
 const IndexPage = () => (
-	<Layout>
-		<SEO title="Pedro Vitorino" />
+  <Layout>
+    <SEO title="Pedro Vitorino" />
     <section id="home">
       <H1 title='Fala pessoal, blz?' emoji='👋' />
-      <Par>
+      <Par marginBottom={true}>
         Meu nome e Pedro e sou desenvolvedor frontend. Estou no mundo da tencologia desde 2009, e mais recentemente tenho me aprofundado no universo frontend.
       </Par>
-      <Par>
-        Sou entusiasta do universo da programaçao, e nos ultimos anos tenho desenvolvido grande interesse por arquitetura front end e design de codigo.
-      </Par>
-      <Par>
-        Tenho muito interesse nas comunidades e participo ativamente de eventos, meetups e outra atividades. Tenho escrevido palestras sobre minhas experiencias
-      </Par>
+      <div className='d-flex align-center mb-lg'>
+        <Par>
+          Sou entusiasta do universo da programaçao, e nos ultimos anos tenho desenvolvido grande interesse por arquitetura front end e design de codigo.
+        </Par>
+        <div className='fig-1'>
+          <Geoform type={'triangle'} size={'lg'}></Geoform>
+        </div>
+      </div>
+      <div className='d-flex align-center mb-lg'>
+        <div className='fig-2'>
+          <Geoform type={'rectangle'} size={'sm'}></Geoform>
+        </div>
+        <Par>
+          Tenho muito interesse nas comunidades e participo ativamente de eventos, meetups e outra atividades. Tenho escrevido palestras sobre minhas experiencias
+        </Par>
+      </div>
     </section>
     <section id="work">
-      <H1 title='Com o que trabalho' emoji='💼' />
-      <Par>
+      <div className='d-flex align-center mb-md'>
+        <div className='fig-3'>
+          <Geoform type={'pacman'} size={'lg'}></Geoform>
+        </div>
+        <H1 title='Com o que trabalho' emoji='💼' />
+      </div>
+      <Par marginBottom={true}>
         Hoje atuo como desenvolvedor frontend no <a href='https://socialbank.com.br' target="_blanck" rel="noreferrer">Social Bank</a>. Trabalho principalmente com o framework Angular. Tambem ja atuei com desenvolvimento backend com a linguagem Java e C#. Alem disso ja trabalhei
       </Par>
       <BoxList boxType='top'>
@@ -70,6 +86,9 @@ const IndexPage = () => (
           <li>Typescript / Javascript / RxJS</li>
         </ul>
       </BoxList>
+      <div className='fig-4'>
+        <Geoform type={'circle'} size={'lg'}></Geoform>
+      </div>
     </section>
     <section id="education">
       <H1 title='Formacao academica' emoji='🎓' />
@@ -77,17 +96,17 @@ const IndexPage = () => (
         Especialista em Analise e Desenvolvimento de Sistemas para Gestao de Negocios pelo Instituto Federal do Triangulo Mineiro. Alem disso sou bacharel em Sistemas de Informaçao pela Universidade Federal de Uberlandia.
       </Par>
       <Par>
-      Como trabalho de conclusao da pos graduacao utilizei <strong>Python</strong> e tecnicas de analise de dados para gerar um perfil de reprovacao dos alunos de computacao do Brasil.
+        Como trabalho de conclusao da pos graduacao utilizei <strong>Python</strong> e tecnicas de analise de dados para gerar um perfil de reprovacao dos alunos de computacao do Brasil.
       </Par>
     </section>
     <section id="contents">
       <H1 title='Projetos pessoais' emoji='💭' />
       {
-        contents.map((content, i) => 
-          (<ContentBox key={i} type={content.type} title={content.title} content={content.content} url={content.url}/>))
+        contents.map((content, i) =>
+          (<ContentBox key={i} type={content.type} title={content.title} content={content.content} url={content.url} />))
       }
     </section>
-	</Layout>
+  </Layout>
 )
 
 export default IndexPage
