715f7f96b96e18a3ad82b3cf3465f115fcd85754
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-09-23 14:04:59 -0300

    feat: add initial i18n config

---
+++ src/helpers/i18n.ts
@@ -0,0 +1,38 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const Languages = ['en', 'pt']
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      pt: {
+          Index: require('../locales/pt/Index.json'),
+          Layout: require('../locales/pt/Layout.json')
+      },
+      en: {
+        Index: require('../locales/en/Index.json'),
+        Layout: require('../locales/en/Layout.json')      
+      }
+    },
+    fallbackLng: 'en',
+    preload: Languages,
+    supportedLngs: Languages,
+    ns: ["Index, Layout"],
+    returnObjects: true,
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false
+    },
+    react: {
+      wait: true,
+    },
+  });
+
+i18next.languages = Languages;
+
+export default i18next;
